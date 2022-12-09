91038184dbb314fb5acb1087368e6c63979708a7
jobst <j.obst@teb-akademia.pl>
2022-12-09 17:51:37 +0100

    2022-12-08 TreetopTreeHouse Part01

---
+++ app/Year2022/Day08TreetopTreeHouse/Part01.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Year2022\Day08TreetopTreeHouse;
+
+use App\Tools\FileReader;
+use Exception;
+use function array_filter;
+use function array_search;
+use function count;
+use function sizeof;
+use function var_dump;
+
+class Part01
+{
+    public function __construct(
+        private readonly FileReader $fileReader,
+    )
+    {
+    }
+
+    /**
+     * @throws Exception
+     */
+    public function run(): int
+    {
+        $file = $this->fileReader->readFile(dirname(__FILE__) . '/input.txt');
+
+        $result = $this->calculateResult($file);
+
+        $this->fileReader->closeFile($file);
+
+        return $result;
+    }
+
+    private function calculateResult(mixed $file): int
+    {
+        $rows = [];
+
+        while (($line = fgets($file)) !== false) {
+            $line = str_replace("\n", "", $line);
+            $rows[] = str_split($line);
+        }
+
+        return $this->getNumberOfVisibleTress($rows);
+    }
+
+    private function getNumberOfVisibleTress(array $rows): int
+    {
+        $visibleTrees = 0;
+
+        foreach ($rows as $rowKey => $columns) {
+            foreach ($columns as $colKey => $treeHeight) {
+                if ($this->treeIsVisible($rows, $rowKey, $colKey, $treeHeight)) {
+                    $visibleTrees++;
+                }
+            }
+        }
+
+        return $visibleTrees;
+    }
+
+    private function treeIsVisible(array $rows, int $rowKey, int $colKey, int $treeHeight): bool
+    {
+        $forestWidth = count($rows[$rowKey]);
+        $forestLength = count($rows);
+
+        $visibleFrom = [
+            'left' => true,
+            'right' => true,
+            'top' => true,
+            'bottom' => true,
+        ];
+
+        for ($i = 0; $i < $colKey; $i++) {
+            if ($rows[$rowKey][$i] >= $treeHeight) {
+                $visibleFrom['left'] = false;
+                break;
+            }
+        }
+
+        for ($i = $colKey+1; $i < $forestWidth; $i++) {
+            if ($rows[$rowKey][$i] >= $treeHeight) {
+                $visibleFrom['right'] = false;
+                break;
+            }
+        }
+
+        for ($i = 0; $i < $rowKey; $i++) {
+            if ($rows[$i][$colKey] >= $treeHeight) {
+                $visibleFrom['top'] = false;
+                break;
+            }
+        }
+
+        for ($i = $rowKey+1; $i < $forestLength; $i++) {
+            if ($rows[$i][$colKey] >= $treeHeight) {
+                $visibleFrom['bottom'] = false;
+                break;
+            }
+        }
+
+        return sizeof(array_filter($visibleFrom)) > 0;
+    }
+}
--- index.php
+++ index.php
@@ -1,11 +1,11 @@
 <?php
 
-use App\Year2022\Day07NoSpaceLeftOnDevice\Part02;
+use App\Year2022\Day08TreetopTreeHouse\Part01;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $container = new DI\Container();
-$handler = $container->get(Part02::class);
+$handler = $container->get(Part01::class);
 $result = $handler->run();
 
 var_dump($result);
